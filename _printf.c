34c6fc356bd7ab141b1d99ce7014898854e8cc07
BraianPerez97 <3603@holbertonschool.com>
2023-03-28 23:40:15 +0000

    initial commit

---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _printf - function that prints
+ * @format: argument vector  array.
+ * Return: character count. 
+ */
+int _printf(const char *format, ...)
+{
+
+}
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef _PRINTF_
+#define _PRINTF_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int _printf(const char *format, ...);
+#endif
