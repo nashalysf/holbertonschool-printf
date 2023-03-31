87471e07f0d8801ad6d662e4c6fb4dbc9c84e275
BraianPerez97 <3603@holbertonschool.com>
2023-03-31 02:23:47 +0000

    print should be workning

---
+++ _test_/main.h
@@ -0,0 +1,13 @@
+#ifndef _PRINTF_
+#define _PRINTF_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdarg.h>
+int _putchar(char c);
+int _printf(const char *format, ...);
+int _write(char c);
+#endif
+
